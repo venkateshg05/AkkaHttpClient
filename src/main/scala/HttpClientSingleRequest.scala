eafbbc94e33d819cf2b438c78c285241595c68e3
venkateshg05 <venkatesh_gopal@outlook.com>
2022-11-02 05:18:05 -0500

    (feat) print response body

--- src/main/scala/HttpClientSingleRequest.scala
+++ src/main/scala/HttpClientSingleRequest.scala
@@ -8,28 +8,36 @@ import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshal
 
-import scala.concurrent.Future
-import scala.util.{ Failure, Success }
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
 object HttpClientSingleRequest {
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem(Behaviors.empty, "SingleRequest")
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.executionContext
+    val start_time = args(0)
+    val time_delta = args(1)
 
     val responseFuture: Future[HttpResponse] = Http().singleRequest(
       HttpRequest(
         method = HttpMethods.POST,
-        uri = "https://iglc5ilpn7.execute-api.us-east-1.amazonaws.com/Prod/checktimewithinbounds",
-        entity = HttpEntity(ContentTypes.`application/json`, "{\"start_time\":\"22-45\",\"time_delta\":\"10\"}")
+        uri = "https://nvnwt4ripg.execute-api.us-east-1.amazonaws.com/Prod/getLogs",
+        entity = HttpEntity(
+          ContentTypes.`application/json`,
+          s"{\"start_time\":" + s"\"$start_time\"" + ",\"time_delta\":" + s"\"$time_delta\"" + "}"
+        )
       )
     )
 
-    responseFuture
-      .onComplete {
-        case Success(res) => println(res)
-        case Failure(_)   => sys.error("something wrong")
-      }
+    val timeout = 5.second
+    val responseAsString = Await.result(
+      responseFuture.flatMap(resp => Unmarshal(resp.entity).to[String]),
+      timeout
+    )
+
+    println(responseAsString)
   }
 }
